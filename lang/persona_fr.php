d96f8df545048b4cf1ace5023c69d88a17b66be7
Cerdic <cedric@yterium.com>
2013-06-29 21:42:51 +0200

    nettoyage (copie/colle) et un message en cas de redirection

--- lang/persona_fr.php
+++ lang/persona_fr.php
@@ -11,18 +11,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'message_js_authorize_popup' => 'Pour poursuivre, merci d\'autoriser les popups',
 	'message_js_unexpected_error' => 'Erreur innatendue : ',
 
-	'titre_formulaire_finish_signup' => 'Choisissez un pseudo et indiquez votre email',
-	'label_nom' => 'Pseudo',
-	'label_email' => 'Courriel (<span lang="en">email</span>)',
-	'bouton_terminer' => 'Terminer',
-	'info_signup_ok' => 'Votre compte a bien été créé !',
-	'info_confirmer_email_deja_utilise' => 'Vous êtes déjà inscrit avec l\'adresse <b>@email@</b>.<br />
-Vous allez recevoir un email avec un lien de confirmation pour finir de vous connecter et associer votre compte <b>@social_source@</b>.',
-
-	'titre_mail_confirm_signup' => 'Connexion avec @social_source@',
-	'bouton_me_connecter' => 'Me connecter',
-	'info_finir_inscription_suivre_lien' => 'Pour finir votre identification, suivez ce lien : @url_confirm@',
-	'confirmsignup_titre_email' => 'Terminer votre identification',
+	'message_succes_redirige' => 'Identification réussie, vous allez être redirigé…',
 );
 
 ?>
