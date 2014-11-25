3f038e7370baf120cb6d77c10b2b1d7d85850364
Cerdic <cedric@yterium.com>
2014-11-25 15:09:15 +0100

    mise a jour slogan et description

--- lang/paquet-magiclogin_fr.php
+++ lang/paquet-magiclogin_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'magiclogin_description' => 'Login rapide, sans mot de passe, par Facebook, Twitter ou email (en secours)',
+	'magiclogin_description' => 'Login rapide, sans mot de passe, par Facebook, Twitter, Google ou Persona',
 	'magiclogin_nom' => 'MagicLogin',
-	'magiclogin_slogan' => 'Login simplifié par Twitter, Facebook ou email',
+	'magiclogin_slogan' => 'Login simplifié par Facebook, Twitter, Google ou Persona',
 );
 
 ?>
